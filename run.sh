6d055af5eb0efc3d62109823e3d49392dc07bf52
Doan Bui <bxdoan93@gmail.com>
2023-05-28 20:27:31 +0700

    script

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 s=$BASH_SOURCE ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ; SCRIPT_HOME="$s"  # get SCRIPT_HOME=executed script's path, containing folder, cd & pwd to get container path
-HOME_REPO="$SCRIPT_HOME/.."
-HOME_SCRIPT="$HOME_REPO/app/scripts"
+HOME_REPO="$SCRIPT_HOME"
 
 if [[ -f "$HOME/.pyenv/shims/pipenv" ]]; then
   pipenv="$HOME/.pyenv/shims/pipenv"
@@ -16,8 +15,8 @@ fi
 # if not found pipenv run python directly
 if [[ -z "$pipenv" ]]; then
   echo "pipenv not found, run python directly"
-  PYTHONPATH=`pwd` python3 "$HOME_SCRIPT/steal.py" "$@"
+  PYTHONPATH=`pwd` python3 "$HOME_REPO/steal.py" "$@"
 else
-  PYTHONPATH=`pwd` $pipenv run python3 "$HOME_SCRIPT/steal.py" "$@"
+  PYTHONPATH=`pwd` $pipenv run python3 "$HOME_REPO/steal.py" "$@"
 fi
 
